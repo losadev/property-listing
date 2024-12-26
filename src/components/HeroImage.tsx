7f3764b671546c0ba0f5f546f37c4494198e1159
losadev <losadauresdev@gmail.com>
2024-12-26 11:52:32 +0100

    feat: bar filter styles done

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,10 +1,12 @@
 import '../styles/HeroImage.css';
+import FilterBar from './FilterBar';
 
 const HeroImage = () => {
   return (
     <div className="container-hero-image">
         <h1>Peace, nature, dream</h1>
         <p>Find and book a great experience</p>
+        <FilterBar />
     </div>
   );
 }
---
+++ src/components/Slider.tsx
@@ -0,0 +1,14 @@
+import '../styles/Slider.css';
+
+const Slider = () => {
+  return (
+    <>
+    <label className="switch">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+    </label>
+    </>
+  )
+}
+
+export default Slider;
